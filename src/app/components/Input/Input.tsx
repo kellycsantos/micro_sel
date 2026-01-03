3cb7313ab30db370b6032d574874462233196a07
Kelly C Santos <kellycristinasntos@gmail.com>
2026-01-03 19:17:47 -0300

    feat: created input

---
+++ src/app/components/Input/Input.tsx
@@ -0,0 +1,23 @@
+import { InputMask, Replacement } from '@react-input/mask'
+import { InputHTMLAttributes } from 'react'
+import styles from './input.module.scss'
+import { Tooltip } from '../Tooltip/Tooptip'
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+    label?: string,
+    mask?: string,
+    replacement?: Replacement,
+    tooltip?: string
+    onChange?: (e: any) => void,
+
+}
+export const Input = ({ label, mask, replacement, onChange, tooltip, ...rest }: InputProps) => {
+    return (
+
+        <label className={styles.label}>
+            <p> {label} {tooltip && <Tooltip content={tooltip} />}</p>
+            <InputMask {...rest} mask={mask} replacement={replacement} separate onChange={onChange} className={styles.input} />
+        </label>
+
+    )
+}
