ff58c0326f12f7898ceb2e1c986a6e4f7256d90e
Kelly C Santos <kellycristinasntos@gmail.com>
2026-01-04 02:00:36 -0300

    feat: created custom hook and add utils

--- src/app/components/ResumeContainer/ResumeContainer.tsx
+++ src/app/components/ResumeContainer/ResumeContainer.tsx
@@ -1,21 +1,22 @@
+'use client'
 import styles from './resume_container.module.scss'
 import { FileText } from 'lucide-react'
 
+import { useResume } from './hooks/useResumeContainer'
 
-
-type ListItems = {
+export type ListItems = {
     idFatura: number,
     value: number,
     discount: number,
     resume: number
 }
 
-type ResumeProps = {
+export type ResumeProps = {
     listItems: ListItems,
-
 }
 
-export const ResumeContainer = ({listItems}: ResumeProps) => {
+export const ResumeContainer = ({ listItems }: ResumeProps) => {
+    const use = useResume({ listItems })
     return (
         <div className={styles.resume_container}>
             <h2>Ödeme Özeti</h2>
@@ -31,29 +32,24 @@ export const ResumeContainer = ({listItems}: ResumeProps) => {
                     </li>
                     <li>
                         <p>Discount</p>
-                        <p>{listItems?.discount}%</p>
+                        <p>{use?.discountValue}%</p>
                     </li>
                     <li>
                         <p>Total</p>
-                        <p>R$ {listItems?.resume}</p>
+                        <p>R$ {use?.valueWithDiscount}</p>
                     </li>
-
                 </ul>
             </div>
-
 
             <footer className={styles.total_container}>
                 <span className={styles.total}>
                     <p>Ödenecek Tutar</p>
-                    <p>$ {listItems?.resume}</p>
+                    <p>$ {use?.valueWithDiscount}</p>
                 </span>
-
                 <span>
                     <FileText size={48} />
                 </span>
-
             </footer>
-
         </div>
     )
 }
