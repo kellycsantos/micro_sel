211ad5020c65e42b64227a0e6e71ad7facd809b4
Kelly C Santos <kellycristinasntos@gmail.com>
2026-01-04 19:45:01 -0300

    feat: created convert money

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,14 @@
-export const convertPercentValue = (value:number) => {
+
+export const convertPercentValue = (value: number, ) => {
    return Number(value.toString().split('.')[1])
 }
+
+export const convertMoney = (value: any, lang?: string) => {
+   return  new Intl.NumberFormat(lang ?? 'pt-BR', {
+         minimumFractionDigits: 2,
+         maximumFractionDigits: 2,
+      }).format(value)
+
+   
+}
+   
